2012bf949b8b5d1e9fc3c70ba45f1ce52d18ad07
Eric Randriamparany <randriamparanyeric@gmail.com>
2026-05-03 21:37:45 +0300

    amelioration estethique

--- obj/Debug/net8.0/Tontine.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tontine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tontine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b203e9822b1d11cacd9030445beba1f22d18d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d725cec74e07e63c2391538bfce106f7cf2782d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tontine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tontine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
